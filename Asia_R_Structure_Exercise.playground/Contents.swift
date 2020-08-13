66040d567f5a236b7e0aff5b0d235d4c1811c7a7
Asiarv25 <67976040+Asiarv25@users.noreply.github.com>
2020-08-13 14:13:07 -0500

    Add files via upload

---
+++ Asia_R_Structure_Exercise.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+struct EngineTypes {
+    var V5 = 5
+    var V6 = 6
+    var V7 = 7
+    var V8 = 8
+}
+
+let myEngineTypes = EngineTypes()
+
+print("The engine types available for you to pick from are:")
+
+print("Version\(myEngineTypes.V5)")
+print("Version\(myEngineTypes.V6)")
+print("Version\(myEngineTypes.V7)")
+print("Version\(myEngineTypes.V8)")
